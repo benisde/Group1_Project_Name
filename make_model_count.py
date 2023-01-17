feabb7e53e17e94e50e71de1dbb8729ddd4c1912
dileepa <dileeparanjan@gmail.com>
2023-01-16 23:02:47 -0500

    line chart for the max 20 records

--- make_model_count.py
+++ make_model_count.py
@@ -9,12 +9,13 @@ def bar():
     vehicle_df = vehicle_df[["RECALL_NUMBER_NUM", "MAKE_NAME_NM","MODEL_NAME_NM"]]
     vehicle_df = vehicle_df.groupby(["MAKE_NAME_NM","MODEL_NAME_NM"]).count()
     vehicle_df = vehicle_df.sort_values(by="RECALL_NUMBER_NUM")
+    vehicle_df = vehicle_df[vehicle_df.RECALL_NUMBER_NUM >= 17]
 
-    vehicle_df.plot(figsize=(20, 4))
+    vehicle_df.plot.bar(figsize=(20, 4))
 
-    return vehicle_df.tail(20)
+    return vehicle_df
 
 
 # Filtering 
-#vehicle_df.drop(vehicle_df[vehicle_df.RECALL_NUMBER_NUM == 18].index, inplace=True)
+#vehicle_df.drop(vehicle_df[vehicle_df.RECALL_NUMBER_NUM > 18].index, inplace=True)
 #vehicle_df.tail(20)
